2175936a6b3176687024cb8d5822f538ed43b49f
mdallen5393 <allen_matthew@outlook.com>
2023-07-21 14:14:35 -0500

    Task 1 attempt

--- utils/db.js
+++ utils/db.js
@@ -7,49 +7,23 @@ class DBClient {
     const database = process.env.DB_DATABASE || 'files_manager';
     this.url = `mongodb://${host}:${port}/${database}`;
     this.client = new MongoClient(this.url, { useUnifiedTopology: true });
-    this.db = null;
-  }
-
-  async connect() {
-    try {
-      await this.client.connect();
-      this.db = this.client.db();
-    } catch (err) {
-      console.error(err);
-    }
+    this.client.connect();
   }
 
   async isAlive() {
-    // if (!this.db) {
-    //   await this.connect();
-    // }
-
-    // try {
-    //   const serverStatus = await this.db.command({ serverStatus: 1 });
-    //   return serverStatus.ok === 1;
-    // } catch (err) {
-    //   console.error(err);
-    //   return false;
-    // }
-    return false
+    return this.client.isConnected();
   }
 
   async nbUsers() {
-    if (!this.db) {
-      await this.connect();
-    }
-
-    const collection = this.db.collection('users');
+    const db = this.client.db();
+    const collection = db.collection('users');
     const count = await collection.countDocuments();
     return count;
   }
 
   async nbFiles() {
-    if (!this.db) {
-      await this.connect();
-    }
-
-    const collection = this.db.collection('files');
+    const db = this.client.db();
+    const collection = db.collection('files');
     const count = await collection.countDocuments();
     return count;
   }
