572fbd7c4a5cd42785b1ca7cc164cc41dfa26436
mdallen5393 <allen_matthew@outlook.com>
2023-07-21 13:55:36 -0500

    Task 1 attempt

--- utils/db.js
+++ utils/db.js
@@ -2,21 +2,12 @@ const { MongoClient } = require('mongodb');
 
 class DBClient {
   constructor() {
-    this.host = process.env.DB_HOST || 'localhost';
-    this.port = process.env.DB_PORT || 27017;
-    this.database = process.env.DB_DATABASE || 'files_manager';
-    this.url = `mongodb://${this.host}:${this.port}`;
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    this.url = `mongodb://${host}:${port}/${database}`;
     this.client = new MongoClient(this.url, { useUnifiedTopology: true });
-    this.db = null;
-  }
-
-  async connect() {
-    try {
-      await this.client.connect();
-      this.db = this.client.db(this.database);
-    } catch (err) {
-      console.error(err);
-    }
+    this.client.connect();
   }
 
   async isAlive() {
@@ -24,33 +15,17 @@ class DBClient {
   }
 
   async nbUsers() {
-    if (!this.db) {
-      await this.connect();
-    }
-
-    try {
-      const usersCollection = this.db.collection('users');
-      const count = await usersCollection.countDocuments();
-      return count;
-    } catch (err) {
-      console.error(err);
-      return 0;
-    }
+    const db = this.client.db();
+    const collection = db.collection('users');
+    const count = await collection.countDocuments();
+    return count;
   }
 
   async nbFiles() {
-    if (!this.db) {
-      await this.connect();
-    }
-
-    try {
-      const filesCollection = this.db.collection('files');
-      const count = await filesCollection.countDocuments();
-      return count;
-    } catch (err) {
-      console.error(err);
-      return 0;
-    }
+    const db = this.client.db();
+    const collection = db.collection('files');
+    const count = await collection.countDocuments();
+    return count;
   }
 }
 
