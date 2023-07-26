ce4ad4b24f0464fe826870ea00e47bef14c396a8
chostler24 <connor.hostler@gmail.com>
2023-07-26 15:01:36 -0500

    files controller rework again

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -1,79 +1,76 @@
-const fs = require('fs');
-const path = require('path');
-const { v4: uuidv4 } = require('uuid');
-const dbClient = require('../utils/db');
-const redisClient = require('../utils/redis');
-const { FOLDER_PATH } = process.env;
+import fs from 'fs';
+import { v4 as uuidv4 } from 'uuid';
+import path from 'path';
+import { ObjectID } from 'mongodb';
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
 
+const FOLDER_PATH = process.env.FOLDER_PATH || '/tmp/files_manager';
 const FilesController = {
   async postUpload(req, res) {
-    const token = req.header('X-Token');
-
-    if (!token) {
-      return res.status(401).json({ error: 'Unauthorized' });
-    }
-
-    const key = `auth_${token}`;
-    const userId = await redisClient.get(key);
-
+    // Retrieve user based on the token
+    const token = req.headers['x-token'];
+    const userId = await redisClient.get(`auth_${token}`);
     if (!userId) {
       return res.status(401).json({ error: 'Unauthorized' });
     }
-
-    const { name, type, parentId, isPublic, data } = req.body;
-
+    // Check for required fields
+    const {
+      name, type, isPublic = false, data, parentId = 0,
+    } = req.body;
     if (!name) {
       return res.status(400).json({ error: 'Missing name' });
     }
-
     if (!type || !['folder', 'file', 'image'].includes(type)) {
       return res.status(400).json({ error: 'Missing type' });
     }
-
     if (type !== 'folder' && !data) {
       return res.status(400).json({ error: 'Missing data' });
     }
 
+    // Check parentId
     if (parentId) {
-      const parentFile = await dbClient.getFileById(parentId);
-
+      const parentFile = await dbClient.db.collection('files').findOne({ _id: ObjectID(parentId) }); // <-- Change this line
       if (!parentFile) {
         return res.status(400).json({ error: 'Parent not found' });
       }
-
       if (parentFile.type !== 'folder') {
         return res.status(400).json({ error: 'Parent is not a folder' });
       }
     }
-
-    const file = {
-      userId,
-      name,
-      type,
-      isPublic: !!isPublic,
-      parentId: parentId || 0,
+    const fileData = {
+      userId, name, type, isPublic: isPublic || false, parentId: parentId || 0,
     };
 
     if (type === 'folder') {
-      const newFile = await dbClient.createFile(file);
-      return res.status(201).json(newFile);
-    } else {
-      const storingFolderPath = FOLDER_PATH || '/tmp/files_manager';
-      const localPath = path.join(storingFolderPath, uuidv4());
-
-      if (!fs.existsSync(storingFolderPath)) {
-        fs.mkdirSync(storingFolderPath, { recursive: true });
+      const existingFolder = await dbClient.db.collection('files').findOne({ name, type: 'folder', parentId: parentId || 0 });
+      if (existingFolder) {
+        return res.status(400).json({ error: 'Folder already exists' });
       }
-
-      const fileData = Buffer.from(data, 'base64');
-
-      fs.writeFileSync(localPath, fileData);
-
-      file.localPath = localPath;
-      const newFile = await dbClient.createFile(file);
-      return res.status(201).json(newFile);
+      const result = await dbClient.db.collection('files').insertOne(fileData);
+      return res.status(201).json({
+        id: result.insertedId, userId, name, type, isPublic: fileData.isPublic, parentId,
+      });
     }
+    // Save the path
+    const fileUuid = uuidv4();
+    const localPath = path.join(FOLDER_PATH, fileUuid);
+    // Ensure the folder exists
+    if (!fs.existsSync(FOLDER_PATH)) {
+      fs.mkdirSync(FOLDER_PATH, { recursive: true });
+    }
+    // Save the file
+    fs.writeFileSync(localPath, data, { encoding: 'base64' });
+    fileData.localPath = localPath;
+    const result = await dbClient.db.collection('files').insertOne(fileData);
+    return res.status(201).json({
+      id: result.insertedId,
+      userId,
+      name,
+      type,
+      isPublic: fileData.isPublic,
+      parentId,
+    });
   },
 };
-
 module.exports = FilesController;
